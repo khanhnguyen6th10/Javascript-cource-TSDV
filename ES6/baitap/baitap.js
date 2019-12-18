36a59e2140de8eb9cc20719edc20c33b8bded45e
unknown <khanhnq62@wru.vn>
2019-12-18 12:05:21 +0700

    js-file

---
+++ ES6/baitap/baitap.js
@@ -0,0 +1,79 @@
+// Bai 1: tinh tong cac so tu 1 den n
+// let n = 3;
+// let sum = 0;
+// for (let i = 1; i<= n; i++){
+//     sum = sum +i;
+// }
+// console.log(`Ket qua la: ${sum}`);
+
+
+
+// Bai 2: tinh giai thua cua n
+// let n = 0;
+// let giaithua = 1;
+// if (n<0){
+//     console.log(`Error`);
+// }else{
+//     for (let i = 1; i<=n; i++){
+//         giaithua = giaithua*i;
+//     }
+//     console.log(`Ket qua la: ${giaithua}`);
+// }
+
+
+
+// Bai 5: tim phan tu lon nhat trong mang 5 phan tu
+// let array = [1,-2,3,4,5];
+// let max = [0];
+// for (let i in array){
+//     if (max < array[i]){
+//         max = array[i];
+//     }
+// }
+// console.log(`Ket qua la: ${max}`);
+
+
+
+// Bai 6: tim phan tu nho nhat trong mang 5 phan tu
+// let array = [1,2,3,4,5];
+// let min = [99];
+// for (let i in array){
+//     if (min > array[i]){
+//         min = array[i];
+//     }
+// }
+// console.log(`Ket qua la: ${min}`);
+
+
+
+// Bai 8: tinh tong cac phantu trong mang 
+// let array = [1,2,3,4,5,9,8];
+// let sum = 0;
+// for (let i in array){
+//     sum = sum + array[i];
+// }
+// console.log(`Ket qua la: ${sum}`);
+
+
+
+// Bai 9: tinh trung binh cong cac phantu trong mang
+// let array = [1,2,3,4,5,9,8];
+// let sum = 0;
+// let count = 0;
+// for (let i in array){
+//     sum = sum + array[i];
+//     count++;
+// }
+// console.log(`Ket qua la: ${sum/count}`);
+
+
+
+// Bai 4: tinh tong cac chu so cua n 
+// let n = 123;
+// let sum = 0;
+// while (n!=0) {
+//     let tachso = n % 10;
+//     n /= 10
+//     sum = sum + tachso;
+// }
+// console.log(`Ket qua la: ${sum}`);
