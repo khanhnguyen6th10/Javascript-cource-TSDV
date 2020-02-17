af860a33cdda612be1ed55cc3622749d807cac3a
khanhnguyen6th10 <54888020+khanhnguyen6th10@users.noreply.github.com>
2020-02-17 16:20:47 +0700

    reactjs todolist

---
+++ src/Components/ToDoList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export class ToDoList extends Component {
+    componentDidUpdate() {
+        this.props.inputElement.current.focus()
+      }
+    render(){
+        return(
+            <div className="todoListMain">
+            <div className="header">
+              <form onSubmit={this.props.addItem}>
+                <input
+                  placeholder="Task"
+                  ref={this.props.inputElement}
+                  value={this.props.currentItem.text}
+                  onChange={this.props.handleInput}
+                />
+                <button type="submit"> Add Task </button>
+              </form>
+            </div>
+          </div>
+        )
+    }
+}
+
+
+
+export default ToDoList
